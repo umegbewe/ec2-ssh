ff863ebf94b07947f8bd29bc05115b07795a522c
Umegbewe Nwebedu <nwebedujunior55@gmail.com>
2022-12-08 18:27:14 -0600

    directory flag and handle key pairs without .pem prefix

--- sshprobe.go
+++ sshprobe.go
@@ -15,8 +15,12 @@ import (
 	"fmt"
 )
 
-var instance []string
-
+var ( 
+	instance []string
+	err error
+	user = flag.String("user", "ubuntu", "Username to use")
+	directory = flag.String("directory", "~/.ssh/", "Directory to find ssh keys")
+)
 
 func GetInstances() ([]ec2.Instance, error) {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
@@ -68,8 +72,15 @@ func GetInstances() ([]ec2.Instance, error) {
 func ssh(keyname string, user string, address string) error {
 	var err error
 
-	fmt.Println("ssh", "-o ConnectTimeout=5", user+"@"+address, "-i", "~/.ssh/"+keyname)
-	cmd := exec.Command("ssh", "-o ConnectTimeout=5", user+"@"+address, "-i", "~/.ssh/"+keyname)
+	filename := *directory+"/"+keyname+".pem"
+
+	/* handle key pair's that might not have the .pem prefix*/
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		filename = *directory+"/"+keyname
+	}
+
+	fmt.Println("ssh", "-o ConnectTimeout=5", user+"@"+address, "-i", filename)
+	cmd := exec.Command("ssh", "-o ConnectTimeout=5", user+"@"+address, "-i", filename)
 
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
@@ -120,7 +131,7 @@ func Filter() []ec2.Instance {
 		privateIPAddress := strings.Split(instance, "|")[0]
 
 		privateIPAddress = strings.TrimSpace(privateIPAddress)
-
+		
 		for _, i := range instances {
 			if *i.PrivateIpAddress == privateIPAddress {
 				filteredInstances = append(filteredInstances,  i)
@@ -132,10 +143,12 @@ func Filter() []ec2.Instance {
 }
 
 func main() {
-	var user = flag.String("user", "ubuntu", "Username to use")
 	flag.Parse()
 	selectedInstances := Filter()
 	for _, instance := range selectedInstances {
+		// if *instance.PublicIpAddress == "<nil>" {
+		// 	err = ssh(*instance.KeyName, *user, *instance.PrivateIpAddress)
+		// }
 		err := ssh(*instance.KeyName, *user, *instance.PublicIpAddress)
 		if err != nil {
 			err = ssh(*instance.KeyName, *user, *instance.PrivateIpAddress)
