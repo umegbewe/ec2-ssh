97d25141ac8f19db21ace95eedf3e8317d34ac5f
Umegbewe Nwebedu <nwebedujunior55@gmail.com>
2022-12-09 10:31:39 -0600

    helper: get tag names func

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,11 +1,25 @@
 package helpers
 
+import "github.com/aws/aws-sdk-go/service/ec2"
+
 func StrOrDefault(s *string, defaultVal string) string {
 	if s == nil {
 		return defaultVal
 	} else {
 		return *s
 	}
+}
+
+func GetTagName(inst *ec2.Instance) []string {
+
+	var nameValues []string
+
+	for _, t := range inst.Tags {
+		if *t.Key == "Name" {
+			nameValues = append(nameValues, *t.Value)
+		}
+	}
+	return nameValues
 }
 
 func filter(slice []string, predicate func(string) bool) []string {
